5049535cc359528c4ca3431f99afe08016cc73e9
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-10-07 13:21:42 -0600

    Edited the meta description of the application

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,9 @@ import GoogleAnalytics from './components/GoogleAnalytics'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Golden Ratio Calculator App',
-  description: 'Tool to calculate the golden ratio of a number',
+  title: 'Golden Ratio Calculator',
+  description: 'Golden Ratio Calculator - A simple and intuitive tool to calculate and visualize the golden ratio for any given number. Ideal for designers, artists, and enthusiasts seeking the harmony of the golden proportion.',
+  
 }
 
 export default function RootLayout({ children }) {
